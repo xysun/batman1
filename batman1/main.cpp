381cf35f27d7357583fc4ea2bbd60357e069c498
Sun, Joy (Agoda) <joy.sun@agoda.com>
2016-07-17 14:26:50 +0700

    first commit

---
+++ batman1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  batman1
+//
+//  Created by Sun, Joy (Agoda) on 7/17/2559 BE.
+//  Copyright © 2559 Xiayun Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
